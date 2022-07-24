9f5c7fbde53eb5b569afcc3c14ac081a443205d1
konstantinosrizos <rizos.dev@gmail.com>
2022-07-24 18:58:09 +0100

    Initial Project commit

---
+++ ExpenseTracker/ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Konstantinos Rizos on 24/7/22.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
