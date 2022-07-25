a22b658425c10a5afc11efb5d70b8cb3b0591808
konstantinosrizos <rizos.dev@gmail.com>
2022-07-25 18:19:07 +0100

    Added more extensions

--- ExpenseTracker/ExpenseTracker/TransactionRow.swift
+++ ExpenseTracker/ExpenseTracker/TransactionRow.swift
@@ -11,12 +11,30 @@ struct TransactionRow: View {
 	var transaction: Transaction
     var body: some View {
 		HStack(spacing: 20) {
+			RoundedRectangle(cornerRadius: 20, style: .continuous)
+				.fill(Color.icon.opacity(0.3))
+				.frame(width: 44, height: 44)
 			VStack(alignment: .leading, spacing: 6) {
 				Text(transaction.merchant)
 					.font(.subheadline)
 					.bold()
 					.lineLimit(1)
+				
+				Text(transaction.category)
+					.font(.footnote)
+					.opacity(0.7)
+					.lineLimit(1)
+				
+				Text(transaction.dateParsed, format: .dateTime.year().month().day())
+					.font(.footnote)
+					.foregroundColor(.secondary)
 			}
+			
+			Spacer()
+			
+			Text(transaction.signedAmount, format: .currency(code: "USD"))
+				.bold()
+				.foregroundColor(transaction.type == TransactionType.credit.rawValue ? Color.text : .primary)
 		}
 		.padding([.top, .bottom], 8)
     }
