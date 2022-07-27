199499835e5378cc9bd25d1365800dffe4d9e00f
konstantinosrizos <rizos.dev@gmail.com>
2022-07-27 18:10:30 +0100

    Data Fetcher method created

--- ExpenseTracker/ExpenseTracker/ExpenseTrackerApp.swift
+++ ExpenseTracker/ExpenseTracker/ExpenseTrackerApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct ExpenseTrackerApp: App {
+	@StateObject var transactionListViewModel = TransactionListViewModel()
+	
     var body: some Scene {
         WindowGroup {
             ContentView()
+				.environmentObject(transactionListViewModel)
         }
     }
 }
