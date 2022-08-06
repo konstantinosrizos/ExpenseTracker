69ca65fa18cf6c64dcf69a28286cecfbff8a4851
konstantinosrizos <rizos.dev@gmail.com>
2022-08-06 19:42:56 +0100

    Added the Chart View

--- ExpenseTracker/ExpenseTracker/ContentView.swift
+++ ExpenseTracker/ExpenseTracker/ContentView.swift
@@ -6,8 +6,11 @@
 //
 
 import SwiftUI
+import SwiftUICharts
 
 struct ContentView: View {
+	@EnvironmentObject var transactionListViewModel: TransactionListViewModel
+	
     var body: some View {
         NavigationView {
             ScrollView {
@@ -16,6 +19,22 @@ struct ContentView: View {
                     Text("Overview")
                         .font(.title2)
                         .bold()
+					
+					let data = transactionListViewModel.accumulateTransactions()
+					
+					if !data.isEmpty {
+						let totalExpenses = data.last?.1 ?? 0
+						CardView {
+							VStack(alignment: .leading) {
+								ChartLabel(totalExpenses.formatted(.currency(code: "USD")), type: .title, format: "$%.02f")
+								LineChart()
+							}
+							.background(Color.systemBackground)
+						}
+						.data(data)
+						.chartStyle(ChartStyle(backgroundColor: Color.systemBackground, foregroundColor: ColorGradient(Color.icon.opacity(0.4), Color.icon)))
+						.frame(height: 300)
+					}
 					
 					RecentTransactionList()
                 }
